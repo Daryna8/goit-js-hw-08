9754585dd1e6cf7de3f2c1825acdc53316c35fd7
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-11-07 17:59:04 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeVideoElem = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeVideoElem);
+
+player.on('timeupdate', throttle(onTimeUpdate, 250));
+
+function onTimeUpdate(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+  console.log(data.seconds);
+}
+
+const savedDataSeconds = localStorage.getItem("videoplayer-current-time");
+console.log(savedDataSeconds);
+    
+player.setCurrentTime(savedDataSeconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
